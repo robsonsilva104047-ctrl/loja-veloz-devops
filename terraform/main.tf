97900db5d7df3610c165a28ebafd64fc8d79f1e1
robsonsilva104047-ctrl <robson.silva.104047@a.fecaf.com.br>
2026-05-27 08:26:43 -0300

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "veloz_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "veloz-production-vpc"
+  }
+}
+
+resource "aws_eks_cluster" "veloz_cluster" {
+  name     = "veloz-kubernetes-prod"
+  role_arn = "arn:aws:iam::123456789012:role/EKSClusterRole"
+
+  vpc_config {
+    subnet_ids = ["subnet-abc12345", "subnet-def12345"]
+  }
+}
